fa86edb143fe72b22572df75933ab83effea80b1
Riddhi <riddhicjain242@gmail.com>
2025-10-07 11:09:14 +0530

    Initial commit: Nova eCommerce platform

---
+++ routes/authRoute.js
@@ -0,0 +1,35 @@
+import express from "express";
+import { forgotPasswordController, registerController, testController, updateProfileController } from "../controllers/authController.js";
+import { loginController } from "../controllers/authController.js";
+import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
+
+//router object
+const router = express.Router()
+
+//routing
+//REGISTER || METHOD POST
+router.post('/register', registerController);
+
+//LOGIN || POST
+router.post('/login', loginController);
+
+//Forgot Password || POST
+router.post('/forgot-password', forgotPasswordController)
+
+//test routes
+router.get('/test', requireSignIn, isAdmin, testController);
+
+//Protected User route auth
+router.get('/user-auth', requireSignIn, (req,res) => {
+    res.status(200).send({ ok: true });
+});
+
+//Protected Admin route auth
+router.get("/admin-auth", requireSignIn, isAdmin, (req,res) => {
+    res.status(200).send({ ok: true });
+});
+
+//update profile
+router.put('/profile', requireSignIn, updateProfileController);
+
+export default router;
